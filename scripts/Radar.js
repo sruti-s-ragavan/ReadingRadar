bb40d43cd958a43afa0faa446edf9daa82550a28
Sruti <ss2@abcam.com>
2013-05-01 20:58:55 +0530

    Quadrant name added to radar

--- scripts/Radar.js
+++ scripts/Radar.js
@@ -1,5 +1,7 @@
-function Radar(renderedOnCanvas) {
-	var canvas = renderedOnCanvas;
+function Radar(renderOnCanvas, dataToRender) {
+	var canvas = renderOnCanvas;
+	var data = dataToRender;
+
 	this.render = function(){
 		this.drawQuadrants();
 		this.drawCircles();
@@ -45,7 +47,122 @@ function Radar(renderedOnCanvas) {
 	}
 
 	this.drawQuadrantNames = function(){
-		
+		var quadrantMap = {
+			1 : {
+				"left" : {
+					"dimension" : canvas.width / 2.0,
+					"direction" : 0
+				},
+				"right" : {
+					"dimension" : canvas.width,
+					"direction" : -1
+				},
+				"top" : {
+					"dimension" : 0,
+					"direction" : +1
+				},
+				"bottom" : {
+					"dimension" : canvas.height / 2.0,
+					"direction" : 0
+				},
+				"FarEdges" : {
+					"X" : "right",
+					"Y" : "top"
+				}
+			},
+
+			2 : {
+				"left" : {
+					"dimension" : canvas.width / 2.0,
+					"direction" : 0
+				},
+				"right" : {
+					"dimension" : canvas.width,
+					"direction" : -1
+				},
+				"top" : {
+					"dimension" : canvas.height / 2.0,
+					"direction" : 0
+				},
+				"bottom" : {
+					"dimension" : canvas.height,
+					"direction" : -1
+				},
+				"FarEdges" : {
+					"X" : "right",
+					"Y" : "bottom"
+				}			
+			},
+
+			3 : {
+				"left" : {
+					"dimension" : 0,
+					"direction" : +1
+				},
+				"right" : {
+					"dimension" : canvas.width / 2.0,
+					"direction" : 0
+				},
+				"top" : {
+					"dimension" : canvas.height / 2.0,
+					"direction" : 0
+				},
+				"bottom" : {
+					"dimension" : canvas.height,
+					"direction" : -1
+				},
+				"FarEdges" : {
+					"X" : "left",
+					"Y" : "bottom"
+				}				
+			},
+
+			4 : {
+				"left" : {
+					"dimension" : 0,
+					"direction" : +1
+				},
+				"right" : {
+					"dimension" : canvas.width / 2.0,
+					"direction" : 0
+				},
+				"top" : {
+					"dimension" : 0,
+					"direction" : +1
+				},
+				"bottom" : {
+					"dimension" : canvas.height / 2.0,
+					"direction" : 0
+				},
+				"FarEdges" : {
+					"X" : "left",
+					"Y" : "top"
+				}				
+			}
+
+		}
+
+		var quadrants = _.keys(quadrantMap);
+		var context = canvas.getContext("2d");
+
+		_.each(quadrants, function(quadrant){
+			var quadrantName = data[quadrant].quadrantName;
+			var edges = quadrantMap[quadrant];
+			
+			//todo : hard-coded number 5 
+			var xOffset = (quadrantName.length * 5) + 10;
+			var farXEdge = edges[edges.FarEdges.X];
+			var textX = farXEdge.dimension + (farXEdge.direction * xOffset);
+
+			//todo : hard-coded number 10
+			var yOffset= 10;
+			var farYEdge = edges[edges.FarEdges.Y];
+			var textY = farYEdge.dimension + (farYEdge.direction * yOffset);
+
+			context.fillText(quadrantName, textX, textY);
+
+		})
+
 	}
 
 }
--- scripts/reading-radar.js
+++ scripts/reading-radar.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	//todo : see if browser understands html5
 
 	var canvas = $("#radar")[0];
-	var radar = new Radar(canvas);
+	var radar = new Radar(canvas, Data);
 
 	radar.render();
 	
